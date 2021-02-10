6affa0fac4a6225898d52fcec4aad6b505a18156
misclikd <christi1427@gmail.com>
2021-02-09 22:03:48 -0500

    updated systemintegrity_script.ps1

--- systemintegrity_script.ps1
+++ systemintegrity_script.ps1
@@ -10,10 +10,8 @@ ForEach-Object {
         Hash = (Get-Filehash $_.fullname -algorithm md5).hash
 }}
 
-if ($result.length -lt 1) {
-        # write-host "no files found"
-    } else {
-        $result | Format-Table -auto | Out-File res.txt
+if ($result.length -gt 0) {
+        # $result | Format-Table -auto | Out-File res.txt
         $result | Format-Table -auto 
     }
 Write-Host "files found: "  $result.length
